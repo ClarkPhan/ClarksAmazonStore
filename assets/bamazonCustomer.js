39080e702abfbb020eab394c0b8fa45188788362
Clark <clarkphan@github.com>
2018-01-06 13:46:22 -0800

    implement queryData function to print product table out

--- assets/bamazonCustomer.js
+++ assets/bamazonCustomer.js
@@ -6,11 +6,26 @@ var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '',
+  password: 'manohman',
   database: 'bamazon'
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log('connected');
+  console.log('-------------------------------------');
+  queryData();
 });
+
+function queryData() {
+  connection.query("SELECT * FROM products", function (err,res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log('Item: ' + res[i].item_id);
+      console.log('Stock: ' + res[i].stock_quantity.toString().magenta);
+      console.log('Price: ' + res[i].price.toString().green);
+      console.log('Product: ' + res[i].product_name.red);
+      console.log('Department: ' + res[i].department_name.cyan);
+      console.log('-------------------------------------');
+    }
+  })
+}
