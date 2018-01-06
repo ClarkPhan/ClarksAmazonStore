2be0cb28c5003534c6ad909c8968ac14c67a20f2
Clark <clarkphan@github.com>
2018-01-06 13:36:04 -0800

    add connection test

--- assets/bamazonCustomer.js
+++ assets/bamazonCustomer.js
@@ -1,2 +1,16 @@
 // Import mysql
 var mysql = require('mysql');
+var color = require('colors');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'bamazon'
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('connected');
+});
