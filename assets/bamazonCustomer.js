acd0eaf35b9d042cc93a9edf7d7dfba8fdd26a69
Clark <clarkphan@github.com>
2018-01-06 14:27:28 -0800

    add selectProduct function to select product by item id

--- assets/bamazonCustomer.js
+++ assets/bamazonCustomer.js
@@ -1,12 +1,17 @@
 // Import mysql
 var mysql = require('mysql');
+
+// Import colors
 var color = require('colors');
+
+// Read and set environment variables
+var dotenv = require("dotenv").config();
 
 var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'manohman',
+  password: process.env.pw,
   database: 'bamazon'
 });
 
@@ -16,7 +21,14 @@ connection.connect(function(err) {
 
 function queryData() {
   connection.query("SELECT * FROM products", function (err,res) {
+    print(res);
+  })
+}
+
+function print(res) {
+  for (var i = 0; i < res.length; i++) {
     for (var i = 0; i < res.length; i++) {
+      console.log('-------------------------------------');
       console.log('Item: ' + res[i].item_id);
       console.log('Stock: ' + res[i].stock_quantity.toString().magenta);
       console.log('Price: ' + res[i].price.toString().green);
@@ -24,9 +36,16 @@ function queryData() {
       console.log('Department: ' + res[i].department_name.cyan);
       console.log('-------------------------------------');
     }
+  }
+}
+
+function selectProduct(id) {
+  connection.query("SELECT * FROM products WHERE item_id =?", [id], function(err,res) {
+    print(res);
   })
 }
 
 module.exports = {
-  queryData: queryData
+  queryData: queryData,
+  selectProduct: selectProduct
 } 
